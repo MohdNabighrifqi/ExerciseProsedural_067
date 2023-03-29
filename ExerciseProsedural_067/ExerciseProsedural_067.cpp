84fa8f85b34a515f709747f574b9f10f34cdd4cb
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-03-29 09:38:00 +0700

    memasukan string status 1

--- ExerciseProsedural_067/ExerciseProsedural_067.cpp
+++ ExerciseProsedural_067/ExerciseProsedural_067.cpp
@@ -4,3 +4,10 @@ using namespace std;
 double rerata(double a, double b) {
     return (a + b) / 2;
 }
+
+string status1(double rata) {
+    if (rata >= 70)
+        return "diterima";
+    else
+        return "ditolak";
+}
