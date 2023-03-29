564369c47cd522e17bec69c8e1f91b1eaf54b390
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-03-29 10:42:17 +0700

    integer main

--- ExerciseProsedural_067/ExerciseProsedural_067.cpp
+++ ExerciseProsedural_067/ExerciseProsedural_067.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
-double rerata(double a, double b) {
-    return (a + b) / 2;
-}
+int main() {
+    const int jumlahKandidat = 5;
+    string nama[jumlahKandidat];
+    string status[jumlahKandidat];
 
-string status1(double rata) {
-    if (rata >= 70)
-        return "diterima";
-    else
-        return "ditolak";
-}
+    for (int i = 0; i < jumlahKandidat; i++) {
+        cout << "Masukkan nama kandidat " << i + 1 << ": ";
+        cin >> nama[i];
 
-string status2(double rata, double nilM) {
-    if (rata >= 70 || nilM >= 80)
-        return "diterima";
-    else
-        return "ditolak";
+        double nilaiMatematika, nilaibahasa_inggris;
+
+        cout << "Masukan nilai Matematika: ";
+        cin >> nilaiMatematika;
+
+        cout << "Masukan nilai bahasa inggris: ";
+        cin >> nilaibahasa_inggris;
+
+        double rataRata = (nilaiMatematika + nilaibahasa_inggris) / 2.0;
+        if (rataRata >= 70.0 && nilaiMatematika > 80.0) {
+            status[i] = "diterima";
+        }
+        else {
+            status[i] = "tidak diterima";
+        }
+    }
+
+    cout << "================" << endl;
+    cout << "                " << endl;
+    cout << left << setw(10) << "Status" << "Name" << endl;
+    cout << "---------------------" << endl;
+    for (int i = 0; i < jumlahKandidat; i++) {
+        cout << left << setw(10) << nama[i] << status[i] << endl;
+    }
+
+    return 0;
 }
+cout << left << setw(10) << nama[i] << status[i] << endl;
